ddceacd4a178c7e1434de90826790696554e2f48
Florian Delezenne <del.florian@gmail.com>
2015-02-16 11:03:46 +0100

    Chargement de Font awesome

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ module.exports = function(app, baseDir, config) {
         app.use(config.baseUrl, express.static(path.join(__dirname, 'dist')));
     }
 
-    app.use(config.baseUrl+ '/scripts/template.js', express.static(path.join(baseDir, config.scenarioTemplate)));
-    app.use(config.baseUrl+ '/scripts/constants.js', express.static(path.join(baseDir, config.constants)));
+    app.use(config.baseUrl + '/scripts/template.js', express.static(path.join(baseDir, config.scenarioTemplate)));
+    app.use(config.baseUrl + '/scripts/constants.js', express.static(path.join(baseDir, config.constants)));
 
     app.get(config.baseUrl + '/scripts/server-config.js', function (req, res) {
         res.type('application/javascript').send('window.serverConfig = ' + JSON.stringify(config.serverConfig || {}) + ';');

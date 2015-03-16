29ccedc384f7b3458567b4f75e232b7e39bea242
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-03-16 19:20:07 +0100

    Clean code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var favicon = require('serve-favicon');
 
 
-module.exports = function(app, baseDir, config) {
+module.exports = function (app, baseDir, config) {
     var servedDirectory = 'app';
 
     if ('production' === app.get('env')) {
@@ -22,11 +22,11 @@ module.exports = function(app, baseDir, config) {
     app.use(config.baseUrl, express.static(servedDirectory));
     app.use(config.baseUrl + '/scripts/template.js', express.static(path.join(baseDir, config.scenarioTemplate)));
     app.use(config.baseUrl + '/scripts/constants.js', express.static(path.join(baseDir, config.constants)));
-    app.get(config.baseUrl + '/scripts/server-config.js', function(req, res) {
+    app.get(config.baseUrl + '/scripts/server-config.js', function (req, res) {
         res.type('application/javascript').send('window.serverConfig = ' + JSON.stringify(config.serverConfig || {}) + ';');
     });
     app.use(favicon(path.join(servedDirectory, 'favicon.ico')));
-    app.route(config.baseUrl + '/*').get(function(req, res) {
+    app.route(config.baseUrl + '/*').get(function (req, res) {
         res.sendFile(path.join(servedDirectory, 'index.html'));
     });
-}
+};

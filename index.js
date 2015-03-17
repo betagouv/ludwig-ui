f0d8d1393743cae2b1ffec862f0361141ee63709
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-17 17:37:53 +0100

    Remove unused server-config/serverConfig transmission mechanism

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ module.exports = function (app, baseDir, config) {
     app.use(config.baseUrl, express.static(servedDirectory));
     app.use(config.baseUrl + '/scripts/template.js', express.static(path.join(baseDir, config.scenarioTemplate)));
     app.use(config.baseUrl + '/scripts/constants.js', express.static(path.join(baseDir, config.constants)));
-    app.get(config.baseUrl + '/scripts/server-config.js', function (req, res) {
-        res.type('application/javascript').send('window.serverConfig = ' + JSON.stringify(config.serverConfig || {}) + ';');
-    });
     app.use(favicon(path.join(servedDirectory, 'favicon.ico')));
     app.route(config.baseUrl + '/*').get(function (req, res) {
         res.sendFile(path.join(servedDirectory, 'index.html'));

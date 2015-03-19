1788ce072be55c1306596381826b3903b8daacec
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-19 18:14:18 +0100

    Unify naming with ludwig-api

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var express = require('express');
 var favicon = require('serve-favicon');
 
 
-module.exports = function (config) {
-    config.baseApiPath = config.baseApiPath || config.baseUrl;
+module.exports = function (options) {
+    options.baseApiPath = options.baseApiPath || options.baseUrl;
 
 
     var app = express(),
@@ -17,23 +17,23 @@ module.exports = function (config) {
         // prerender.io
         app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN).set('protocol', 'https'));
     } else {
-        app.use(config.baseUrl + '/styles', express.static(path.join(__dirname, '.tmp', 'styles')));  // ugly hack to serve compiled SCSS; you will need to `grunt build` every time you change a style
+        app.use(options.baseUrl + '/styles', express.static(path.join(__dirname, '.tmp', 'styles')));  // ugly hack to serve compiled SCSS; you will need to `grunt build` every time you change a style
     }
 
     servedDirectory = path.join(__dirname, servedDirectory);
 
 
-    app.use(config.baseUrl, express.static(servedDirectory));
-    app.use(config.baseUrl + '/scripts/template.js', express.static(config.scenarioTemplatePath));
-    app.get(config.baseUrl + '/scripts/constants.js', function (req, res) {
+    app.use(options.baseUrl, express.static(servedDirectory));
+    app.use(options.baseUrl + '/scripts/template.js', express.static(options.scenarioTemplatePath));
+    app.get(options.baseUrl + '/scripts/constants.js', function (req, res) {
         res.type('application/javascript')
-           .send('angular.module("ludwigConstants", []).constant("config", ' +  // make config available to Angular's dependency management system
-                 JSON.stringify(config) +
+           .send('angular.module("ludwigConstants", []).constant("config", ' +  // make options available to Angular's dependency management system
+                 JSON.stringify(options) +
                  ');'
                 );
     });
     app.use(favicon(path.join(servedDirectory, 'favicon.ico')));
-    app.route(config.baseUrl + '/*').get(function (req, res) {
+    app.route(options.baseUrl + '/*').get(function (req, res) {
         res.sendFile(path.join(servedDirectory, 'index.html'));
     });
 

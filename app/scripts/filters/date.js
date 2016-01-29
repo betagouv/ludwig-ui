c765673d87e48d9733dccf78f9e6ade8f7fc273d
Matti Schneider <hi@mattischneider.fr>
2016-01-29 10:03:43 +0100

    Add missing semicolon

--- app/scripts/filters/date.js
+++ app/scripts/filters/date.js
@@ -4,5 +4,5 @@ angular.module('ludwig')
     .filter('formatDate', function() {
         return function(dateString) {
         	return moment(dateString).format('DD MMMM YYYY');
-        }
+        };
     });
